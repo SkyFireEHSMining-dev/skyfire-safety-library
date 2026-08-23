f5834cc89a8c5a399208cde7948429948cdb5a0d
Nicholas Ryan Murphy <40476454+EmperorBaoBaoofsui@users.noreply.github.com>
2026-08-23 10:49:32 -0700

    Refresh enforcement resource cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const SHELL_CACHE_NAME = "skyfire-quality-cache-v16";
+const SHELL_CACHE_NAME = "skyfire-quality-cache-v17";
 const DATA_CACHE_NAME = "skyfire-regulatory-data-v1";
-const CACHE_VERSION = "quality-v16";
+const CACHE_VERSION = "quality-v17";
 
 const SHELL_CACHE = [
   "./",
